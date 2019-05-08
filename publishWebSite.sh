d7c8d883b118920f36d680cd41be11b95fed8b12
Peter Camps <peter.camps@ugent.be>
2019-05-08 08:36:37 +0200

    add script to publish web site

---
+++ publishWebSite.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# (use "chmod +rx scriptname" to make script executable)
+#
+# This script publishes the SKIRT web site to the Ghent University web host.
+#
+# Instructions:
+#   - use on Mac OS X only
+#   - you need write access to the SKIRT area of the Ghent University web host
+#   - first run the stageWebSite.sh script
+#   - run this script with "git" as default directory
+#
+# This script assumes that:
+#    (a) the stage directory contains the possibly updated web site to be published
+#    (b) the public directory contains an exact mirror (including time stamps) of the currently published web site
+#
+# The script proceeds in two steps:
+#   (1) copy the contents from stage to public, updating only files that were actually changed,
+#       using checksums to verify this rather than time stamps
+#   (2) copy the contents from public to the web host, updating only files with a different time stamp
+#
+# This procedure allows the expensive step (calculating the checksums) to happen on the local file system,
+# while still restricting the remote copy operation to files that actually did change.
+#
+
+# Mount the webhost at a local mount point
+mkdir -p ../webhost
+mount -t smbfs //pcamps@webhost.ugent.be/_skirt ../webhost
+if [ $? -ne 0 ]
+  then exit
+fi
+
+# Make local copy, using checksums to update only files that were actually changed
+rsync -chr --delete ../stage/ ../public/
+
+# Make remote copy, updating only files with a different time stamp
+rsync -hrtv --delete-after ../public/ ../webhost/WWW/
+
+# Unmount the webhost and remove the mount point
+umount ../webhost
+rm -d ../webhost
